c6f4b751313bab1c707b636ca6c50ba1d49a335a
Alfan Mubarok <n4.labs@mailbox.org>
2023-06-01 10:14:21 +0700

    Add: App controller

---
+++ pkg/adapter/controller/app.go
@@ -0,0 +1,6 @@
+package controller
+
+// AppController represents the main controller for the application.
+type AppController struct {
+	Url interface{ Url } // Url represents the controller interface for URLs.
+}

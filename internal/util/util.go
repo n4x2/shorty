74be37e801f056ac902c8bc20de8b6bc142b936a
Alfan Mubarok <n4.labs@mailbox.org>
2023-06-01 10:10:27 +0700

    Add: ShortenURL utility

---
+++ internal/util/util.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"os"
+)
+
+// ShortenURL generates a shortened URL based on the provided long URL.
+// It generates a random hash and takes the first 8 characters of the hash as the shortened URL.
+// If the baseURL environment variable is set, it is appended to the shortened URL.
+func ShortenURL(longURL string) string {
+	hash := generateRandomHash()
+
+	// Take the first 8 characters of the hash as the shortened URL
+	shortURL := hash[:8]
+
+	baseURL := os.Getenv("baseURL")
+	if baseURL != "" {
+		shortURL = baseURL + "/" + shortURL
+	}
+
+	return shortURL
+}
+
+func generateRandomHash() string {
+	hash := make([]byte, 16)
+	_, err := rand.Read(hash)
+	if err != nil {
+		panic(err) // Handle error appropriately in your application
+	}
+	return hex.EncodeToString(hash)
+}

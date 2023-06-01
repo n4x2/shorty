d0f8a1f9b6bb99080e657e42a0cd7fa0c98f0df5
Alfan Mubarok <n4.labs@mailbox.org>
2023-06-01 10:13:56 +0700

    Add: URL controller

---
+++ pkg/adapter/controller/url.go
@@ -0,0 +1,58 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/n4x2/shorty/internal/util"
+	"github.com/n4x2/shorty/pkg/domain/model"
+	"github.com/n4x2/shorty/pkg/usecase/usecase"
+)
+
+// urlController represents the controller implementation for URLs.
+type urlController struct {
+	urlUsecase usecase.Url
+}
+
+// Url defines the interface for URL controllers.
+type Url interface {
+	GetUrls(c *gin.Context) error
+	CreateUrl(c *gin.Context) error
+}
+
+// NewUrlController creates a new URL controller with the given use case.
+func NewUrlController(us usecase.Url) Url {
+	return &urlController{us}
+}
+
+// GetUrls retrieves all URLs and returns them as a JSON response.
+func (uc *urlController) GetUrls(c *gin.Context) error {
+	var u []*model.Url
+
+	u, err := uc.urlUsecase.List(u)
+	if err != nil {
+		return err
+	}
+
+	c.JSON(http.StatusOK, gin.H{"urls": u})
+	return nil
+}
+
+// CreateUrl creates a new URL based on the provided parameters and returns the created URL as a JSON response.
+func (uc *urlController) CreateUrl(c *gin.Context) error {
+	var params model.Url
+
+	if err := c.ShouldBindJSON(&params); err != nil {
+		return err
+	}
+
+	params.ShortUrl = util.ShortenURL(params.LongUrl)
+
+	_, err := uc.urlUsecase.Create(&params)
+	if err != nil {
+		return err
+	}
+
+	c.JSON(http.StatusOK, gin.H{"shortUrl": params.ShortUrl})
+	return nil
+}

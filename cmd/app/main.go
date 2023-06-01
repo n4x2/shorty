fa0d5cd56787493efdba2eeaab5d8ee1e0568b04
Alfan Mubarok <n4.labs@mailbox.org>
2023-06-01 10:23:33 +0700

    Update: Add server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/n4x2/shorty/pkg/infrastructure/datastore"
+	"github.com/n4x2/shorty/pkg/infrastructure/router"
+	"github.com/n4x2/shorty/pkg/registry"
+)
 
 func main() {
-	fmt.Println("Shorty URL Shortener")
+	// Create a new database connection
+	db := datastore.NewDB()
+
+	// Create a new registry with the database connection
+	r := registry.NewRegistry(db)
+
+	// Create a new Gin router
+	g := gin.New()
+
+	// Initialize the router with the registry's app controller
+	g = router.NewRouter(g, r.NewAppController())
+
+	// Start the server on port
+	g.Run(":8081")
 }
